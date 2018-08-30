e8617d9e0332f1ed66498a28b70514baab31e4a5
godzy <isgodzy@msn.com>
2018-08-30 08:17:13 +0200

    premier comit fin du tp Mcommerce-partie1
    Signed-off-by: godzy <isgodzy@msn.com>

---
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -0,0 +1,125 @@
+package com.ecommerce.microcommerce.web.controller;
+
+import com.ecommerce.microcommerce.dao.ProductDao;
+import com.ecommerce.microcommerce.model.Product;
+import com.ecommerce.microcommerce.web.exceptions.ProduitGratuitException;
+import com.ecommerce.microcommerce.web.exceptions.ProduitIntrouvableException;
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+@Api(description = "API pour les opérations CRUD sur les produits.")
+@RestController
+public class ProductController {
+
+    @Autowired private ProductDao productDao;
+
+    // Récupérer la liste des produits
+
+    @RequestMapping(value = "/Produits", method = RequestMethod.GET)
+    public MappingJacksonValue listeProduits() {
+
+        Iterable<Product> produits = productDao.findAll();
+
+        SimpleBeanPropertyFilter monFiltre = SimpleBeanPropertyFilter.serializeAllExcept("prixAchat");
+
+        FilterProvider listDeNosFiltres =
+                new SimpleFilterProvider().addFilter("monFiltreDynamique", monFiltre);
+
+        MappingJacksonValue produitsFiltres = new MappingJacksonValue(produits);
+
+        produitsFiltres.setFilters(listDeNosFiltres);
+
+        return produitsFiltres;
+    }
+
+    // Récupérer un produit par son Id
+    @ApiOperation(
+            value = "Récupère un produit grâce à son ID à condition que celui-ci soit en stock!")
+    @GetMapping(value = "/Produits/{id}")
+    public Product afficherUnProduit(@PathVariable int id) {
+
+        Product produit = productDao.findById(id);
+
+        if (produit == null)
+            throw new ProduitIntrouvableException(
+                    "Le produit avec l'id " + id + " est INTROUVABLE. Écran Bleu si je pouvais.");
+
+        return produit;
+    }
+
+    // ajouter un produit
+    @PostMapping(value = "/Produits")
+    public ResponseEntity<Void> ajouterProduit(
+            /*@Valid*/ @RequestBody Product product) throws ProduitGratuitException {
+
+        if (product.getPrix() == 0) {
+
+            throw new ProduitGratuitException("0 n'est pas une valeur rentable, voleuuuuur !!");
+        }
+        Product productAdded = productDao.save(product);
+
+        if (productAdded == null) return ResponseEntity.noContent().build();
+
+        URI location =
+                ServletUriComponentsBuilder.fromCurrentRequest()
+                        .path("/{id}")
+                        .buildAndExpand(productAdded.getId())
+                        .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping(value = "/Produits/{id}")
+    public void supprimerProduit(@PathVariable int id) {
+
+        productDao.deleteById(id);
+    }
+
+    @PutMapping(value = "/Produits")
+    public void updateProduit(@RequestBody Product product) {
+
+        productDao.save(product);
+    }
+
+    // Pour les tests
+    @GetMapping(value = "test/produits/{prix}")
+    public List<Product> testeDeRequetes(@PathVariable int prix) {
+
+        return productDao.chercherUnProduitCher(400);
+    }
+
+    // Récupérer les marges de tous les produits listés
+    @GetMapping(value = "/AdminProduits")
+    public List<String> calculerMargeProduit() {
+
+        List<Product> listeProduits = productDao.findAll();
+        List<String> marges = new ArrayList<>();
+
+        for (Product listeProduit : listeProduits) {
+            int marge = listeProduit.getPrix() - listeProduit.getPrixAchat();
+            marges.add(listeProduit.toString() + ":" + marge);
+        }
+        return marges;
+    }
+
+    @GetMapping(value = "/ProduitsTries")
+    public List<Product> trierProduitsParOrdreAlphabetique() {
+
+        List<Product> listeTrieAlphabetique = productDao.findByOrderByNom();
+
+        return listeTrieAlphabetique;
+    }
+}
